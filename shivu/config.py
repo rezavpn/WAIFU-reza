6055b21efe0d56614d7a3dea8c6d0a4b88f1017f
rezavpn <83076350+rezavpn@users.noreply.github.com>
2024-04-21 16:32:39 +0700

    clone

--- shivu/config.py
+++ shivu/config.py
@@ -2,20 +2,21 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    OWNER_ID = 1463920223
+    sudo_users = "1683587530", "5254260722", "6747049506", "5539507956", "6915699640", "1463920223","1666459894"
+    GROUP_ID = "-1002032751462"
+    TOKEN = "6815163005:AAEnQXDqju9C-jRV7x7GyQSmL3bqqt03JDk"
+    mongo_url = "mongodb+srv://reza:reza@rezavpn.0n5u13c.mongodb.net/?retryWrites=true&w=majority"
+    PHOTO_URL = ["https://telegra.ph/file/ff59826943869e919a58d.jpg"]
+    SUPPORT_CHAT = "Origanimegc"
+    UPDATE_CHAT = "risuenoofc"
+    BOT_USERNAME = "Origanime_H_W_GuessBot"
+    OWNER_BOT = "reza01345"
+    CHARA_CHANNEL_ID = "-1002119942993"
+    api_id = "2170226"
+    api_hash = "ed1412c738a861fbda7f1a7aa2b7b43b"
 
-    
+
 class Production(Config):
     LOGGER = True
 
--- shivu/modules/start.py
+++ shivu/modules/start.py
@@ -36,15 +36,14 @@ async def start(update: Update, context: CallbackContext) -> None:
         caption = f"""
         ***Heyyyy...***
 
-***I am An Open Source Character Catcher Bot...​Add Me in Your group.. And I will send Random Characters After.. every 100 messages in Group... Use /guess to.. Collect that Characters in Your Collection.. and see Collection by using /Harem... So add in Your groups and Collect Your harem***
+***I am An Open Source Character Catcher Bot...​Add Me in Your group.. And I will send Random Characters After.. every 100 messages in Group... Use /guess to.. Collect that Characters in Your Collection.. and see Collection by using /collection... So add in Your groups and Collect Your harem***
         """
         
         keyboard = [
             [InlineKeyboardButton("ADD ME", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
-            [InlineKeyboardButton("SUPPORT", url=f'https://t.me/{SUPPORT_CHAT}'),
-            InlineKeyboardButton("UPDATES", url=f'https://t.me/{UPDATE_CHAT}')],
+            [InlineKeyboardButton("JOIN THIS", url=f'https://t.me/{SUPPORT_CHAT}'),
+            InlineKeyboardButton("UPDATE HAREM REQUEST", url=f'https://t.me/{UPDATE_CHAT}')],
             [InlineKeyboardButton("HELP", callback_data='help')],
-            [InlineKeyboardButton("SOURCE", url=f'https://github.com/MyNameIsShekhar/WAIFU-HUSBANDO-CATCHER')]
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
         photo_url = random.choice(PHOTO_URL)
@@ -55,10 +54,9 @@ async def start(update: Update, context: CallbackContext) -> None:
         photo_url = random.choice(PHOTO_URL)
         keyboard = [
             [InlineKeyboardButton("ADD ME", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
-            [InlineKeyboardButton("SUPPORT", url=f'https://t.me/{SUPPORT_CHAT}'),
-            InlineKeyboardButton("UPDATES", url=f'https://t.me/{UPDATE_CHAT}')],
+            [InlineKeyboardButton("JOIN THIS", url=f'https://t.me/{SUPPORT_CHAT}'),
+            InlineKeyboardButton("UPDATE HAREM REQUEST", url=f'https://t.me/{UPDATE_CHAT}')],
             [InlineKeyboardButton("HELP", callback_data='help')],
-            [InlineKeyboardButton("SOURCE", url=f'https://github.com/MyNameIsShekhar/WAIFU-HUSBANDO-CATCHER')]
         ]
         
         reply_markup = InlineKeyboardMarkup(keyboard)
@@ -99,9 +97,8 @@ async def button(update: Update, context: CallbackContext) -> None:
         keyboard = [
             [InlineKeyboardButton("ADD ME", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
             [InlineKeyboardButton("SUPPORT", url=f'https://t.me/{SUPPORT_CHAT}'),
-            InlineKeyboardButton("UPDATES", url=f'https://t.me/{UPDATE_CHAT}')],
+            InlineKeyboardButton("UPDATE HAREM REQUEST", url=f'https://t.me/{UPDATE_CHAT}')],
             [InlineKeyboardButton("HELP", callback_data='help')],
-            [InlineKeyboardButton("SOURCE", url=f'https://github.com/MyNameIsShekhar/WAIFU-HUSBANDO-CATCHER')]
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
 
